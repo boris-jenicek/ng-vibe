cd99069b5035d112013eec8cacbde9fb4b8af350
boris jenicek <boris.jenicek@gmail.com>
2024-04-09 23:39:35 +0200

    fix: button.component.ts

--- libs/common/button/src/core/button/button.component.ts
+++ libs/common/button/src/core/button/button.component.ts
@@ -21,7 +21,7 @@ export class ButtonComponent {
 
   get buildButtonCssString(): string {
     let css =
-      'my-4 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 ';
+      'my-4 w-full text-white  focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center ';
 
     if (this.cssClass) {
       css += this.cssClass;
@@ -32,7 +32,16 @@ export class ButtonComponent {
       css += ' ';
     }
     if (this.buttonType) {
-      css += this.buttonType;
+      switch (this.buttonType) {
+        case 'main-btn':
+          css += 'bg-primary-600 hover:bg-primary-700 focus:ring-primary-300';
+          break;
+        case 'second-btn':
+          css +=
+            'bg-secondary-600 hover:bg-secondary-700 focus:ring-secondary-300';
+          break;
+      }
+
       css += ' ';
     }
     return css;
