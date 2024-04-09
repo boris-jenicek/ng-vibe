fe3526aee84e6c1d68e541c852eb9e99522d4eb5
boris jenicek <boris.jenicek@gmail.com>
2024-04-09 18:11:34 +0200

    fix: -scss path -exposed animation enums

--- libs/toastify/src/index.ts
+++ libs/toastify/src/index.ts
@@ -3,5 +3,6 @@ export * from './enums/toast-type.enum';
 export * from './enums/position.enum';
 export * from './enums/progress-bar.enum';
 export * from './enums/text-align.enum';
+export * from './enums/animation.enum';
 export * from './models/toast.remote-control.model';
 export { PublicToastService as ToastifyService } from './services/public-toast.service';
